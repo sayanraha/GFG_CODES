d464cdbf5e49e9db9fe41c0a86907d12addea525
SAYAN RAHA <109590974+sayanraha@users.noreply.github.com>
2023-09-05 20:43:16 +0530

    Added Solution - GfG to GitHub

---
+++ School/Swap kth elements/swap-kth-elements.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// swap k'th element from beginning and end
+	void swapKth(int *arr, int n, int k) {
+	    // code here
+	    
+	    int i = k - 1 , j = n - k;
+	    
+	    swap(arr[i],arr[j]);
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.swapKth(arr, n, k);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
