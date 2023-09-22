65b8e3fa564d302d0d2656dea7c4e4889f57c354
Sayan Raha <109590974+sayanraha@users.noreply.github.com>
2023-09-22 22:04:32 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int n){
+        //code here
+      long long  int fact = 1;
+        
+        for(int i = n;i >= 1;i--){
+            fact *= i;
+        }
+        return fact;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
