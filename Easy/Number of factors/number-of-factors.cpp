fb353a4864e7ea956c0c0ae31684e65a6113668c
Sayan Raha <109590974+sayanraha@users.noreply.github.com>
2023-09-23 19:34:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Number of factors/number-of-factors.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    int countFactors(int N){
+        //code here
+        int count = 1;
+        
+        for(int i = 1;i <= N/2;i++){
+            
+            if(N % i == 0)count++;
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.countFactors(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
