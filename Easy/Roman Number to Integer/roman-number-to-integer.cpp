14a45d02f657337337962bcf7f2910f2327acd97
Sayan Raha <109590974+sayanraha@users.noreply.github.com>
2023-10-09 20:21:57 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Roman Number to Integer/roman-number-to-integer.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        // code here
+        
+        unordered_map<char,int> val;
+        val['I'] = 1;
+        val['V'] = 5;
+        val['X'] = 10;
+        val['L'] = 50;
+        val['C'] = 100;
+        val['D'] = 500;
+        val['M'] = 1000;
+        
+        int res = 0;
+        
+        for(int i = str.length()-1; i >= 0;i--){
+            
+            if(i < str.length()-1 and val[str[i]] < val[str[i+1]]){
+                
+                res -= val[str[i]];
+            }
+            else{
+                res += val[str[i]];
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}
+// } Driver Code Ends
